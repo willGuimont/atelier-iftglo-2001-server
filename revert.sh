73ea70a5d8deebc3562ba5c3d83eaf869ab0bae2
William Guimont-Martin <wil9876543210@hotmail.com>
2023-07-08 16:46:19 -0400

    first commit

---
+++ revert.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+revert_last_commit() {
+  git reset HEAD~1
+}
+
+exit_status=1
+while [ $exit_status -ne 0 ]; do
+  # Execute le script de test
+  ./script.sh
+  # Sauvegarde le status du script de test
+  exit_status=$?
+
+  if [ $exit_status -ne 0 ]; then
+    echo "The script returned a non-zero exit status ($exit_status). Reverting commit and retrying..."
+    revert_last_commit
+  else
+    echo "The script returned 0 (success)."
+    exit 0
+  fi
+
+  # Add a delay between retries (optional)
+  sleep 1
+done
